a34a55f435e0cbd2baefde6139912637a4780f21
Akram Helil <akramhelil@gmail.com>
2019-01-14 21:38:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   original_array = string.split(" ")
     return_array = []
  original_array.each do |sentence|
-return_array << string.reverse
+return_array << sentence.reverse
   end
   return_array.join("")
 end
